a03da994c493def3a1ffa41e5914716866aef35f
hoakhuat <khuathoa141@gmail.com>
2019-07-21 12:34:07 +0700

    homepage done

--- app/src/main/java/com/example/mymovie/adapter/SliderPagerAdapter.java
+++ app/src/main/java/com/example/mymovie/adapter/SliderPagerAdapter.java
@@ -1,6 +1,7 @@
 package com.example.mymovie.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,10 +9,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.PagerAdapter;
 
 import com.bumptech.glide.Glide;
 import com.example.mymovie.R;
+import com.example.mymovie.activity.MovieDetailActivity;
 import com.example.mymovie.model.MovieResponse;
 
 import java.util.List;
@@ -39,18 +42,30 @@ public class SliderPagerAdapter extends PagerAdapter {
 
     @NonNull
     @Override
-    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+    public Object instantiateItem(@NonNull ViewGroup container, final int position) {
         LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View slideLayout = inflater.inflate(R.layout.slide,null);
-
         ImageView slideImg = slideLayout.findViewById(R.id.slide_img);
         TextView slideText = slideLayout.findViewById(R.id.slide_title);
-//        slideImg.setImageResource(mList.get(position).getImage());
         slideText.setText(mList.get(position).getOriginal_title());
         Glide.with(mContext)
                 .load(mList.get(position).getBackdrop_path())
                 .placeholder(R.drawable.ic_account_circle_black_24dp)
                 .into(slideImg);
+
+        slideImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    MovieResponse cliclekMovie = mList.get(position);
+                    Intent intent = new Intent(mContext, MovieDetailActivity.class);
+                    // send movie information to deatilActivity
+                    intent.putExtra("title",mList.get(position).getOriginal_title());
+                    intent.putExtra("poster_path",mList.get(position).getPoster_path());
+                    intent.putExtra("overview",mList.get(position).getOverview());
+                    intent.putExtra("backdrop_path",mList.get(position).getBackdrop_path());
+                    mContext.startActivity(intent);
+            }
+        });
         container.addView(slideLayout);
         return slideLayout;
     }
